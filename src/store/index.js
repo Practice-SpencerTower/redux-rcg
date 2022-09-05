4c2447ce406a29938b0cb457f0d1dce438c52a21
SpencerTower <spencerjtower1@gmail.com>
2022-09-05 11:46:48 -0700

    adds initial counter slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,31 @@
-import { createStore } from 'redux';
+import { createSlice, TaskAbortError } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, showCounter: true };
+
+// takes object as argument
+// each slice needs a name/identifier
+// redux toolkit uses immer to detect state changes
+// can change aspects of state we want to change
+// state is still immutable
+// immer updates state in the background
+createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        },
+    },
+});
 
 // reducer
 const counterReducer = (state = initialState, action) => {
