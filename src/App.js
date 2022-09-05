9591a996b89aaa8f77568ab08a5f6acdac35a6f4
SpencerTower <spencerjtower1@gmail.com>
2022-09-05 14:42:59 -0700

    adds conditional rendering for auth form and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+import { authActions } from './store';
 
 function App() {
+    const isAuthenticated = useSelector((state) => state.isAuthenticated);
     return (
         <>
-            <Auth />
+            {isAuthenticated && <Auth />}
             <Header />
             <Counter />
         </>
--- src/store/index.js
+++ src/store/index.js
@@ -44,12 +44,13 @@ const authSlice = createSlice({
     },
 });
 
-// action identifiers are automatically created - keys match the method names in reducers
-
 const store = configureStore({
     reducer: counterSlice.reducer,
+    auth: authSlice.reducer,
 });
 
+// action identifiers are automatically created - keys match the method names in reducers
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 // connect react-app to redux store
 export default store;
