2239cd9d48b776ae640b1fd07603c7d211adc616
SpencerTower <spencerjtower1@gmail.com>
2022-09-13 11:20:03 -0700

    modularizes authSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,5 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-
-
-const initialAuthState = {
-    isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true;
-        },
-        logout(state) {
-            state.isAuthenticated = false;
-        },
-    },
-});
-
 const store = configureStore({
     reducer: counterSlice.reducer,
     auth: authSlice.reducer,
