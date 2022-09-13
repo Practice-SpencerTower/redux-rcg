5a472fdfa201f9f2b82b2a27e6c2be97446bf34e
SpencerTower <spencerjtower1@gmail.com>
2022-09-13 11:18:54 -0700

    modularizes counterSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,6 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialCounterState = { counter: 0, showCounter: true };
 
-// takes object as argument
-// each slice needs a name/identifier
-// redux toolkit uses immer to detect state changes
-// can change aspects of state we want to change
-// state is still immutable
-// immer updates state in the background
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialCounterState,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        },
-    },
-});
 
 const initialAuthState = {
     isAuthenticated: false,
